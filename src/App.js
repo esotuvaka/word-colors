7ede9e79ce04ad6484a2fbc85705d5dbc7d07342
PiqqiDesigns <eric.otuvaka@gmail.com>
2022-08-30 19:50:09 -0700

    Build: Auto-updating color fix!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 	const [word, setWord] = useState('');
@@ -13,13 +13,10 @@ function App() {
 		//check whole array. If its length is less that 6 add spaces on the end
 
 		for (const value of wArray) {
-			console.log(value.charCodeAt());
-
 			if (value.charCodeAt() % 2 === 0) {
 				const lastDigStr = String(value.charCodeAt()).slice(-2);
 				const lastDigNum = Number(lastDigStr % 6);
 
-				console.log('% BY 2');
 				if (hArr.length < 6) {
 					hArr.push(lastDigNum.toString());
 				}
@@ -27,7 +24,6 @@ function App() {
 				const lastDigStr = String(value.charCodeAt()).slice(-1);
 				const lastDigNum = Number(Math.round(lastDigStr / 2));
 
-				console.log('% BY 3');
 				if (hArr.length < 6) {
 					hArr.push(lastDigNum.toString());
 				}
@@ -35,7 +31,6 @@ function App() {
 				const lastDigStr = String(value.charCodeAt()).slice(-1);
 				const lastDigNum = Number(Math.floor(lastDigStr / 3));
 
-				console.log('% BY 7');
 				if (hArr.length < 6) {
 					hArr.push(lastDigNum.toString());
 				}
@@ -48,7 +43,6 @@ function App() {
 					sum = Math.round(sum % 5);
 					sum = sum.toString();
 				}
-				console.log('TESTING ' + Math.round(sum * 3.6));
 				if (hArr.length < 6) {
 					hArr.push(sum);
 				}
@@ -57,14 +51,15 @@ function App() {
 
 		hArr.unshift('#');
 		hArr = hArr.join('');
-		console.log('hARR HERE ' + hArr);
 
 		return hArr;
 	}
 
-	const hexStr = Algo(wArray);
+	const hexStr = Algo(wArray); //wArray is the inputted word string converted to array
 
-	console.log('HEXSTR HERE ' + typeof hexStr + hexStr);
+	useEffect(() => {
+		setColor(hexStr);
+	}, [hexStr]);
 
 	return (
 		<div
@@ -74,30 +69,25 @@ function App() {
 		>
 			<div className="flex flex-col bg-white p-4 rounded-3xl border-2 border-black">
 				<h1 className="text-3xl mb-2 w-72 text-center">
-					Enter a sentence, emotion, or idea!
+					Enter a word*, sentence, or an idea!
 				</h1>
 				<input
-					className="border-2 border-black py-2 px-2 rounded"
+					className="border-2 border-black py-2 px-2 rounded text-center"
 					value={word}
 					onChange={(e) => {
 						setWord(e.target.value);
 					}}
 				/>
-				<h2 className="mt-8 text-2xl text-center">HexStr is: {hexStr}</h2>
-				<button
-					onClick={(e) => {
-						setColor(hexStr);
-					}}
-					className="border-2 border-black bg-black text-white mt-8 py-2 rounded font-semibold tracking-wider text-xl"
-				>
-					Click For Color!
-				</button>
-				<div className="flex justify-center items-center w-full">
+				<h2 className="mt-8 text-2xl text-center">Hex is: {hexStr}</h2>
+				<div className="flex flex-col justify-center items-center w-full">
 					<div
 						id="box"
 						className="colorDiv"
 						style={{ backgroundColor: color, transition: 'all 1s ease' }}
 					/>
+					<p className="w-72 mt-8">
+						*If your word has less than 6 letters, add a few spaces after it!
+					</p>
 				</div>
 			</div>
 		</div>
